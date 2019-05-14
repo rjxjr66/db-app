a96c4fe7211bf3ad9488ec0356f19244d3bb314f
hansol <rjxjr55@gmail.com>
2019-05-14 20:42:03 +0900

    init

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
-export class HomePage {
+export class HomePage implements OnInit {
+  photos: any[] = [];
+  constructor(private readonly http: HttpClient) { }
 
+  ngOnInit() {
+    this.http.get('http://localhost:3000/photos')
+    .subscribe((data: any) => {
+      this.photos = data;
+    });
+  }
+
+  showDetail(item) {
+    alert(item.description);
+  }
 }
