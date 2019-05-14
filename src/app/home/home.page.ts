05e396cd647b77d3ab0b7ab865865d1636e57702
hansol <rjxjr55@gmail.com>
2019-05-14 20:49:14 +0900

    save

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -17,6 +17,22 @@ export class HomePage implements OnInit {
     });
   }
 
+  newUser() {
+    const user = {
+      name: '새유저',
+      description: '새 유져 입니다.',
+      field: false,
+      filename: 'string',
+      views: 0,
+      is: true,
+    };
+
+    this.http.post('http://localhost:3000/users', user)
+    .subscribe(_ => {
+      this.photos.push(_);
+    });
+  }
+
   showDetail(item) {
     alert(item.description);
   }
